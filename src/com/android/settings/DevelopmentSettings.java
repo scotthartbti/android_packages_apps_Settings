0c755370e3966bb8f4337846d87b94c29aabeab9
scott <scott.hart.bti@gmail.com>
2016-09-14 00:57:43 -0400

    Revert "Derpage"
    This reverts commit 5299e9ee55d86e3f2fae3a3681e05ae9836e296c.

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1666,7 +1666,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private void writeAnimationScaleOption(int which, ListPreference pref, Object newValue) {
         try {
-            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 0.75f;
+            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : .75;
             mWindowManager.setAnimationScale(which, scale);
             updateAnimationScaleValue(which, pref);
         } catch (RemoteException e) {
