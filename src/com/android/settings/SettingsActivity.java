AICP/packages_apps_Settings

DUI: Initial N checkin
Change-Id: If8d277d124e2844810482a52bfb951549ee8c999

src/com/android/settings/SettingsActivity.java
@@ -64,6 +64,10 @@ import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.accounts.ChooseAccountActivity;
 import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.aicp.AEStartActivity;
+import com.android.settings.aicp.FlingSettings;
+import com.android.settings.aicp.NavbarSettings;
+import com.android.settings.aicp.SmartbarSettings;
+import com.android.settings.aicp.PulseSettings;
 import com.android.settings.applications.AdvancedAppSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.InstalledAppDetails;
@@ -373,6 +377,10 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             AEStartActivity.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 

scotthartbti/android_packages_apps_Settings

Signed-off-by: Scott <scott@ubuntu.com>

src/com/android/settings/SettingsActivity.java
@@ -63,6 +63,10 @@ import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.accounts.ChooseAccountActivity;
 import com.android.settings.accounts.ManagedProfileSettings;
+import com.android.settings.beanstalk.FlingSettings;
+import com.android.settings.beanstalk.NavbarSettings;
+import com.android.settings.beanstalk.SmartbarSettings;
+import com.android.settings.beanstalk.PulseSettings;
 import com.android.settings.applications.AdvancedAppSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.InstalledAppDetails;
@@ -376,6 +380,10 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 